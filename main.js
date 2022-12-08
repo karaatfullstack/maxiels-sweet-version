badfbc261911234e5d36ad5b350ad4bc2391840b
Kara Cavanaugh <kara.cavanaugh@fullstackacademy.com>
2022-12-08 12:51:53 -0500

    stuff

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const {db} = require('./server/db')
 const app = require('./server')
 const PORT = 1337
-
+// this is a change!
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   .then(() => {
     console.log('db synced')
